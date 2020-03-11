ecf5906f503cad0ac3c82254d4b258e32179db51
sttagami2 <sttagami2@gmail.com>
2020-03-11 14:48:07 +0900

    [Finish] 5-5

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -23,6 +23,12 @@ class PostImagesController < ApplicationController
     @post_comment = PostComment.new
   end
 
+  def destroy
+    @post_image = PostImage.find(params[:id])                         # Viewから送られてきたIDを元にDB内の画像を探し見つける
+    @post_image.destroy                                               # DB内で該当するIDの画像を削除する
+    redirect_to post_images_path                                      # 上の処理が終わったらoist_images_pathのページを読み込みユーザに見せる
+  end
+
 private
   def post_image_params
       params.require(:post_image).permit(:shop_name, :image, :caption)
